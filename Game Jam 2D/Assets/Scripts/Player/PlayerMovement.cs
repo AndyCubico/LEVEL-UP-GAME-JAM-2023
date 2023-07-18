267450e82255c2a9221a5632ef6a6ab246756436
AndyCubico <99950309+AndyCubico@users.noreply.github.com>
2023-07-18 13:11:10 +0200

    Player test movement

---
+++ Game Jam 2D/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    private Vector2 movementInput;
+    private Animator playerAnimator;
+
+    [SerializeField]
+    private float speed;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        movementInput = new Vector2(moveX, moveY).normalized;
+
+        playerAnimator.SetFloat("Horizontal", moveX);
+        playerAnimator.SetFloat("Vertical", moveY);
+        playerAnimator.SetFloat("Speed", movementInput.sqrMagnitude);
+    }
+    private void FixedUpdate()
+    {
+        playerRb.MovePosition(playerRb.position + movementInput * speed * Time.fixedDeltaTime);
+    }
+
+}
