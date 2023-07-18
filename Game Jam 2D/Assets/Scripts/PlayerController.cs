39f05e527de24843319a2ccc75367c71cb2b183a
flishflash <jose.antonio.tur@estudiantat.upc.edu>
2023-07-18 13:10:43 +0200

    player move

---
+++ Game Jam 2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+    [SerializeField] private float speed = 6.0f;
+    private bool Up; //Para hacer más tarde el flip del sprite
+    private bool Down; //Para hacer más tarde el flip del sprite
+
+    [SerializeField] Rigidbody2D rigidbody;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+}
