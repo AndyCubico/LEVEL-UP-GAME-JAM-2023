fd55b9337ea2327641c964d1cc7749a5e6930446
flishflash <jose.antonio.tur@estudiantat.upc.edu>
2023-07-19 20:10:01 +0200

    transition

--- Game Jam 2D/Assets/Scripts/ChangeScene.cs
+++ Game Jam 2D/Assets/Scripts/ChangeScene.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ChangeScene : MonoBehaviour
 {
     [SerializeField] private string sceneName;
-    void SceneChange()
+    public void SceneChange()
     {
         Debug.Log("sceneName to load: " + sceneName);
         SceneManager.LoadScene(sceneName);
