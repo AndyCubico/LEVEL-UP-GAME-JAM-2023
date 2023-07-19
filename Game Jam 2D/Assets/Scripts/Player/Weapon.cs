47e7d6f00b5d14f208573a706496e063251f4c2e
AndyCubico <99950309+AndyCubico@users.noreply.github.com>
2023-07-19 17:44:37 +0200

    Input mando weapon

--- Game Jam 2D/Assets/Scripts/Player/Weapon.cs
+++ Game Jam 2D/Assets/Scripts/Player/Weapon.cs
@@ -6,6 +6,8 @@ public class Weapon : MonoBehaviour
 {
     private Camera mainCam;
     private Vector3 mousePos;
+    private Vector2 rightStickInput;
+
     private void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
@@ -20,6 +22,14 @@ public class Weapon : MonoBehaviour
         float rotz = Mathf.Atan2(rotation.y, rotation.x)*Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0, 0, rotz);
+
+        // [Andy] Gamepad control weapon
+        rightStickInput = new Vector2(Input.GetAxis("R_Horizontal"), Input.GetAxis("R_Vertical"));
+        if (rightStickInput.magnitude>0f)
+        {
+            float rotG = Mathf.Atan2(rightStickInput.y, rightStickInput.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, rotG);
+        }
 
         if (transform.rotation.eulerAngles.z < 270.0f && transform.rotation.eulerAngles.z > 90.0f)
         {
